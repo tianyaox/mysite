23dfe6325b55aed3d6778386d658eb40e10cbc20
rui <t.y.xu.cmu@gmail.com>
2014-05-18 14:37:37 -0400

    hello world homepage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'siteframework.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+
 )
