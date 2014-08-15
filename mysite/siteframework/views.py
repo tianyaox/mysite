89f491ff30780a5e67ace7de087cfbc36e3ce94c
rui <t.y.xu.cmu@gmail.com>
2014-08-15 19:37:53 -0400

    contact page

--- mysite/siteframework/views.py
+++ mysite/siteframework/views.py
@@ -8,6 +8,9 @@ def home(request):
 def work(request):
 	return render(request, 'siteframework/work.html')
 
+def contact(request):
+	return render(request, 'siteframework/contact.html')
+
 def cmumap(request):
 	return render(request,'siteframework/cmumap.html')
 
