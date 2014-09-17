b0647973531e8096b969896cf16c92965173b562
rui <t.y.xu.cmu@gmail.com>
2014-09-17 12:31:48 -0500

    fixing firefox box

--- mysite/siteframework/static/siteframework/js/video.js
+++ mysite/siteframework/static/siteframework/js/video.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){
-	$('#wrapper').tubular({videoId: '8kZMVG6N7DE'}); // where idOfYourVideo is the YouTube ID.
-})
+// $(document).ready(function(){
+// 	$('#wrapper').tubular({videoId: '8kZMVG6N7DE'}); // where idOfYourVideo is the YouTube ID.
+// })
